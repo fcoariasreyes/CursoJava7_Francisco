a0ca5fcd1589890a9957c3fcbd2563a2033a8c85
Francisco Arias Reyes <francisco.arias@programando.cl>
2018-10-18 21:11:36 -0300

    commit

--- ClubHipico_Francisco/src/cl/clubhipico/clubhipico_francisco/controller/CarreraController.java
+++ ClubHipico_Francisco/src/cl/clubhipico/clubhipico_francisco/controller/CarreraController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import cl.clubhipico.clubhipico_francisco.object.CarreraObject;
 import cl.clubhipico.clubhipico_francisco.util.DataBaseUtil;
 
-public class CarreraController implements BaseController<CarreraObject>{
+public class CarreraController{
 
 	private DataBaseUtil<CarreraObject> database;
 	
@@ -13,41 +13,7 @@ public class CarreraController implements BaseController<CarreraObject>{
 		database = new DataBaseUtil<CarreraObject>();
 	}
 	
-	@Override
-	public boolean insert(CarreraObject carrera) {
-		// TODO Auto-generated method stub
-		return database.insert(carrera);
-	}
-
-	@Override
-	public boolean update(CarreraObject carrera) {
-		// TODO Auto-generated method stub
-		return database.update(carrera);
-	}
-
-	@Override
-	public boolean delete(CarreraObject carrera) {
-		// TODO Auto-generated method stub
-		return database.delete(carrera);
-	}
-
-	@Override
-	public CarreraObject select(CarreraObject carrera) {
-		// TODO Auto-generated method stub
-		return database.select(carrera);
-	}
-
-	@Override
-	public List<CarreraObject> list(CarreraObject carrera) {
-		// TODO Auto-generated method stub
-		return database.list(carrera);
-	}
-
-	@Override
-	public List<CarreraObject> list() {
-		// TODO Auto-generated method stub
-		return database.list();
-	}
+	
 
 	
 
