00e2a1b8ea6234017c9520ecb0b9bca1f93a3465
Francisco Arias Reyes <francisco.arias@programando.cl>
2018-10-18 21:10:16 -0300

    Commit

--- ClubHipico_Francisco/src/cl/clubhipico/clubhipico_francisco/ClubHipicoDataBaseMain.java
+++ ClubHipico_Francisco/src/cl/clubhipico/clubhipico_francisco/ClubHipicoDataBaseMain.java
@@ -1,46 +1,63 @@
 package cl.clubhipico.clubhipico_francisco;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import cl.clubhipico.clubhipico_francisco.controller.CarreraController;
+import cl.clubhipico.clubhipico_francisco.object.CaballoObject;
 import cl.clubhipico.clubhipico_francisco.object.CarreraObject;
+import cl.clubhipico.clubhipico_francisco.util.DataBaseUtil;
 
 public class ClubHipicoDataBaseMain {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException { //En realidad no es recomendable, por que no se esta controlando la excepcion
 		
 		/*PARA RECIBIR DESDE TECLADO*/
-		Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+		/*Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
 		CarreraController carreraController = new CarreraController(); //controlador con logica
 		
-		/*Pedimos los parametros de entrada de la carrera*/
+		//Pedimos los parametros de entrada de la carrera
 		System.out.println ("CLUB HIPICO");
 		System.out.println ("===========");
         System.out.print ("Nombre carrera: " );
 
         String nombreCarrera = entradaEscaner.nextLine ();;
 
-        /*Instanciamos el objeto con los datos obtenidos desde el teclado*/
+        //Instanciamos el objeto con los datos obtenidos desde el teclado
         CarreraObject carrera = new CarreraObject();
         carrera.setNombre(nombreCarrera);
 
-        /*Usamos el controlador*/
+        //Usamos el controlador
         //accion de insertar
-        carreraController.insert(carrera);
-        
-        if (carreraController==null ^ carrera==null) {
-        	
-        }
-        
-        System.out.println("Carrera recibida "+carrera);
-        
+        */
 		
-        for (int i = 0; i<10; i++) {
+		CaballoObject caballo = new CaballoObject();
+		caballo.setRut(1);
+		caballo.setDv('9');
+		caballo.setNombre("Juanito");
+		caballo.setPeso(400);
+		
+		
+		/*Insert*/
+        String insert = "INSERT INTO CABALLO (RUT,DV,NOMBRE,PESO) "
+        		+ "VALUES ("+caballo.getRut()+Math.round((Math.random()*1000000))+","
+        		+ "'"+caballo.getDv()+"',"
+        		+ "'"+caballo.getNombre()+"',"
+        		+ caballo.getPeso()+")";
         
+        DataBaseUtil db = new DataBaseUtil();
+        db.executarQuery(insert);
+               
+        
+        /*Select*/
+        String query = "SELECT * FROM CABALLO"; //No recomendado con *, escribir columnas. 
+        ResultSet resultset = db.select(query);
+        
+        while (resultset.next()) {
+        	System.out.println("RUT: "+resultset.getString("RUT")+"    Nombre caballo:"+resultset.getString("NOMBRE"));
         }
-        
-        
-       
+        System.out.println("Fin");
         
         
 		
--- ClubHipico_Francisco/src/cl/clubhipico/clubhipico_francisco/controller/CarreraController.java
+++ ClubHipico_Francisco/src/cl/clubhipico/clubhipico_francisco/controller/CarreraController.java
@@ -1,12 +1,17 @@
 package cl.clubhipico.clubhipico_francisco.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 import cl.clubhipico.clubhipico_francisco.object.CarreraObject;
 import cl.clubhipico.clubhipico_francisco.util.DataBaseUtil;
 
 public class CarreraController implements BaseController<CarreraObject>{
 
-	private DataBaseUtil<CarreraObject> database = new DataBaseUtil<CarreraObject>();
+	private DataBaseUtil<CarreraObject> database;
+	
+	public CarreraController() throws ClassNotFoundException, SQLException {
+		database = new DataBaseUtil<CarreraObject>();
+	}
 	
 	@Override
 	public boolean insert(CarreraObject carrera) {
